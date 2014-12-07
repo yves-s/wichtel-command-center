94cf5932e58c7e9d73e260d938df068a8f943842
yves-s <yves.schleich@gmail.com>
2014-12-07 01:43:29 +0100

    Fixed Typo

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var app = angular.module('wichtel');
 
 app.constant('NOTIFICATIONS', {
     success: 'email has been successfully sent',
-    sent: 'Sending in process',
+    sent: 'Sending in progress',
     error: 'An Error Occured',
     warning: 'Add at least 2 Wichtels'
 });
